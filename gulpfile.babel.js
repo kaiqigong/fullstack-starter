dc5103a2f96a83e5d26f1d8039ef8ab48e589d36
kaiqigong <cagegong@gmail.com>
2015-12-09 19:47:13 +0800

    gulp es6 client build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,21 +20,40 @@ gulp.task('less', () => {
 
 gulp.task('es6:server', () => {
   gulp.src(['./src/**/*.js', '!./src/public/**/*.js'])
-  .pipe(sourcemaps.init())
   .pipe(babel())
-  .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('es6', () => {
+  gulp.src(['./src/public/**/*.js'])
+  .pipe(webpack({
+    entry: {
+      index: './src/public/js/index.js',
+      index2: './src/public/js/index2.js',
+    },
+    output: {
+      filename: '[name].js',
+    },
+    module: {
+      loaders: [{
+        test: /\.js$/, loader: 'babel-loader'
+      }]
+    }
+  }))
+  .pipe(gulp.dest('./build/public/js/'))
+  .pipe(livereload());
 });
 
 gulp.task('watch', ()=> {
   gulp.watch('./src/public/css/*.less', ['less']);
+  gulp.watch('./src/public/**/*.js', ['es6']);
 });
 
 gulp.task('develop', function () {
   livereload.listen();
   nodemon({
-    script: 'app.js',
-    ext: 'js coffee jade',
+    script: 'src/app.js',
+    ext: 'js',
     stdout: false
   }).on('readable', function () {
     this.stdout.on('data', function (chunk) {
